ba7f6f1cb102ade39e45c4d7cde4b0c185f39c94
Qalac <officialqalac@gmail.com>
2021-06-07 17:37:06 +0100

    solved deployment issue

--- views/routes.js
+++ views/routes.js
@@ -5,7 +5,7 @@ const {postResponses, getResponses} = require('../contolllers/middlewares');
 router.get("/responses/get", getResponses)
 router.post("/responses/post", postResponses)
 router.get("", function(req, res, next){
-    res.send("Deployed Successfully")
+    res.send("next step is to integrate the bot")
 })
 
 module.exports = router
