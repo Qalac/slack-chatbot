3133e755fdf27bcb289b7783629b81b67144027e
Qalac <officialqalac@gmail.com>
2021-06-12 02:42:30 +0100

    testing new event

--- contolllers/middlewares.js
+++ contolllers/middlewares.js
@@ -3,9 +3,8 @@ const axios = require('axios');
 // const {BOT_TOKEN} = require('../constants');
 
 
-const BOT_TOKEN = xoxb-2142287031523-2150781824772-SmaNN61T5ixvoX9z4RRwVEI0
+const BOT_TOKEN = xoxb-2142287031523-2150781824772-IyebBtrPCP429qShUfb33xW9
 const WEBHOOK = "https://hooks.slack.com/services/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"
-
 
 var getResponses = async(req, res, next) => {
     var responses = await modelInstance.find({}, 'feeling availability hobbies digits_on_number_scale')
