428b48a5357e64002bf945cd30e8554216781f25
Qalac <officialqalac@gmail.com>
2021-06-25 16:42:42 +0100

    added mention feature

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 var mongoose = require('mongoose');
-var express = require('express');
-var bodyParser = require('body-parser');
-var server = express();
+var { App } = require('@slack/bolt');
 
 const {PORT, DB_URL, BOT_TOKEN, SIGNING_SECRET} = require('./constants');
-const routes = require('./views/routes') 
 
-server.use(bodyParser.urlencoded({ extended: true }));
-server.use(express.json());
-server.use('/', routes);
+const app = new App({
+    token: BOT_TOKEN,
+    signingSecret: SIGNING_SECRET
+  });
+
+
+app.message(/^(hi|hello|hey).*/, async ({ context, say }) => {
+    const greeting = context.matches[0];
+    await say(`${greeting}, how are you?`);
+});
 
 mongoose.connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(()=> {
@@ -17,4 +21,19 @@ mongoose.connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
         console.log('error connecting to database')
     })
 
-server.listen(PORT);
+
+(async () => {
+    await app.start(PORT);
+    console.log('⚡️ Bolt app is running!');
+})();
+
+
+
+// var slashResponse = async(req, res, next) => {
+//     res.send(
+//         {
+//             "response_type": "in_channel",
+//             "text": `Welcome <@${req.body.user_name}>. How are you doing?`,
+//         }
+//     );
+// }
