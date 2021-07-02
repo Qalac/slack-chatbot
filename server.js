1709ce2914e2e993a5a186c90d19da89e034328a
Qalac <officialqalac@gmail.com>
2021-07-02 15:20:22 +0100

    dropdown select option

--- server.js
+++ server.js
@@ -2,12 +2,62 @@ var mongoose = require('mongoose');
 var { App } = require('@slack/bolt');
 
 const {PORT, DB_URL, BOT_TOKEN, SIGNING_SECRET} = require('./constants');
-const {EmotionPicker} = require('./widgets');
 
 const app = new App({
     token: BOT_TOKEN,
     signingSecret: SIGNING_SECRET
   });
+
+
+
+const EmotionPicker = {
+	blocks: [
+		{
+			"type": "input",
+			"element": {
+				"type": "static_select",
+				"placeholder": {
+					"type": "plain_text",
+					"text": "Select an item",
+					"emoji": true
+				},
+				"options": [
+					{
+						"text": {
+							"type": "plain_text",
+							"text": "*this is plain_text text*",
+							"emoji": true
+						},
+						"value": "value-0"
+					},
+					{
+						"text": {
+							"type": "plain_text",
+							"text": "*this is plain_text text*",
+							"emoji": true
+						},
+						"value": "value-1"
+					},
+					{
+						"text": {
+							"type": "plain_text",
+							"text": "*this is plain_text text*",
+							"emoji": true
+						},
+						"value": "value-2"
+					}
+				],
+				"action_id": "static_select-action"
+			},
+			"label": {
+				"type": "plain_text",
+				"text": " ",
+				"emoji": true
+			}
+		}
+	]
+}
+
 
 
 app.command("/hello", async ({ command, ack, say }) => {
