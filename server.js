ebf15ed2b2073de8767181b2678dd5dab0ac2905
Qalac <officialqalac@gmail.com>
2021-07-02 15:00:29 +0100

    dropdown select option

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 var { App } = require('@slack/bolt');
 
 const {PORT, DB_URL, BOT_TOKEN, SIGNING_SECRET} = require('./constants');
-const {EmotionPicker} = require('./widgets');
 
 const app = new App({
     token: BOT_TOKEN,
@@ -13,7 +12,25 @@ const app = new App({
 app.command("/hello", async ({ command, ack, say }) => {
     try {
         await ack();
-        say(`Welcome <@${command.user_name}>. How are you doing? \n ${EmotionPicker}`);
+        say({
+            blocks: [{
+                "type": "section",
+                "text": {
+                  "type": "mrkdwn",
+                  "text": "Pick a date for me to remind you"
+                },
+                "accessory": {
+                  "type": "datepicker",
+                  "action_id": "datepicker_remind",
+                  "initial_date": "2019-04-28",
+                  "placeholder": {
+                    "type": "plain_text",
+                    "text": "Select a date"
+                  }
+                }
+              }]
+        })
+        // say(`Welcome <@${command.user_name}>. How are you doing? \n ${EmotionPicker}`);
     }
     catch(err) {
         console.log(err);
